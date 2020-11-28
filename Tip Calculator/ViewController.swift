1f17f2bf0cbcdd7d092d68ec3265a501a5ea20b5
Shi Tao Luo <sluo5991@gmail.com>
2020-11-28 15:47:25 -0500

    Changed Tip Percentage label and Total label to $0.00

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func onTap(_ sender: Any) {
     }
     
